ba02b36de5a2dda2b6add8d37e5241b53794c7a5
BenoitSafari <gregory.michalak.192@gmail.com>
2025-06-17 23:17:07 +0200

    refactor(architecture): Removed Storybook config

--- storybook/config/initialGlobals.ts
+++
@@ -1,5 +0,0 @@
-import type {Preview} from "@storybook/react";
-
-export const initialGlobals: Preview['initialGlobals'] = {
-    theme: 'light',
-};
--- storybook/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './config/decorators';
-export * from './config/globalTypes';
-export * from './config/initialGlobals';
-export * from './config/parameters';
