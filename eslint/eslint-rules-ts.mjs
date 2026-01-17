3c236a283269b8b84cb5fdf8f13b8b5988c09fc1
BenoitSafari <gregory.michalak.192@gmail.com>
2026-01-17 01:49:58 +0100

    eslint sheets

---
+++ eslint/eslint-rules-ts.mjs
@@ -0,0 +1,18 @@
+/** @type {Record<string, any>} */
+export default {
+    '@typescript-eslint/prefer-for-of': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-empty-function': ['warn', { allow: [] }],
+    '@typescript-eslint/consistent-type-definitions': 'warn',
+    '@typescript-eslint/consistent-type-imports': 'warn',
+    '@typescript-eslint/explicit-member-accessibility': 'error',
+    '@typescript-eslint/no-dynamic-delete': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    '@typescript-eslint/no-unsafe-function-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+};
